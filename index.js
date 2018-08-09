a24213ccbc2370be63f8593647887128ea8f3995
Klimenko Ivan <kli-van@yandex.ru>
2018-08-10 01:00:24 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import token from './token';
+
+const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot(token(), { polling: true });
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  let resp = match[1];
+  bot.sendMessage(msg.chat.id, resp)
+  console.log(msg.from.id+' '+msg.from.first_name)
+});
+
+bot.onText(/\/id/, (msg) => {
+  bot.sendMessage(msg.chat.id, msg.chat.id+' - id этого чата')
+  console.log(bot.getChat(msg.chat.id)+' - объект чата')
+  console.log(bot.getChatMembersCount(msg.chat.id)+' - кол-во участников чата')
+  console.log(msg.from.id+' '+msg.from.first_name)
+})
+
+bot.onText(/\/photo (https?:\/\/\S+)/, (msg, match) => {
+  let resp = match[1];
+  bot.sendPhoto(-307924393, resp);
+  bot.sendMessage(-307924393, 'Счастья и хорошего настроения!');
+  console.log(msg.from.id+' '+msg.from.first_name)
+})
+
+bot.onText(/\/sendto (\-[0-9]+|[0-9]+) (\S+.*)/, (msg, match) => {
+  let id = match[1];
+  let text = match[2];
+  bot.sendMessage(id, text);
+  console.log(msg.from.id+' '+msg.from.first_name)
+})
+
