625212baf91cb57c451aab0482f4a0f8a5e1f150
Klimenko Ivan <kli-van@yandex.ru>
2018-08-10 02:43:46 +0300

    id-remember

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bot = new TelegramBot(token(), { polling: true });
 // функция записывает id_имя человека
 function writeWhoAsk(message) {
   let text = message.from.id+' : '+message.from.first_name;
-  fs.writeFile(`id_name/${message.from.id}+'_'+${message.from.id}.txt`, text, function(error){
+  fs.writeFile(`id_name/${message.from.id}_${message.from.id}.txt`, text, function(error){
     if(error) throw error; // если возникла ошибка
   });
 }
