39941854b5683161ec45445732cd8543d875aad2
Klimenko Ivan <kli-van@yandex.ru>
2018-08-10 17:14:20 +0300

    D:/Program Files/Git/help as function-module

--- index.js
+++ index.js
@@ -18,25 +18,23 @@ function stopTimer() {
   timer = null
 }
 
-let help = require('./functions/help')
-
-// function help(message) {
-//   let response = 
-//   `Привет, ${message.from.first_name}. Имеются следующие команды:\n
-//   /echo (text) - повторяет текст
-//   /id - выдает id группового чата и ваш
-//   /photo (link) - пишете команду боту в лс, он шлет фото, размещенное по ссылке, в группу
-//   /sendto (id) (text) - пишете боту в лс id адресата и текст сообщения. При условии, что человек прописал у бота /start, ему придет сообщение с текстом от имени бота
-//   /write (text) - записать текст в файл на сервере
-//   /read - прочесть текст, записанный в файле командой /write
-//   /note - прислать txt файл с текстом, записанным в последний раз командой /write
-//   /timer (number) (number) - пишете команду, желаемый часовой пояс (числом, например +3) и желаемую периодичность оповещений в минутах
-//   /stoptimer - остановить таймер`
-//   bot.sendMessage(message.chat.id, response);
-// }
+function help(message) {
+  let response = 
+  `Привет, ${message.from.first_name}. Имеются следующие команды:\n
+  /echo (text) - повторяет текст
+  /id - выдает id группового чата и ваш
+  /photo (link) - пишете команду боту в лс, он шлет фото, размещенное по ссылке, в группу
+  /sendto (id) (text) - пишете боту в лс id адресата и текст сообщения. При условии, что человек прописал у бота /start, ему придет сообщение с текстом от имени бота
+  /write (text) - записать текст в файл на сервере
+  /read - прочесть текст, записанный в файле командой /write
+  /note - прислать txt файл с текстом, записанным в последний раз командой /write
+  /timer (number) (number) - пишете команду, желаемый часовой пояс (числом, например +3) и желаемую периодичность оповещений в минутах
+  /stoptimer - остановить таймер`
+  bot.sendMessage(message.chat.id, response);
+}
 
 bot.onText(/\/help/, (msg) => {
-  help.help(msg)
+  help(msg)
   writeWhoAsk(msg);
 });
 
@@ -48,7 +46,7 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 
 bot.onText(/\/id/, (msg) => {
   bot.sendMessage(msg.chat.id, msg.chat.id+' - id этого чата');
-  bot.sendMessage(msg.chat.id, msg.chat.from+' - а это ваш id');
+  bot.sendMessage(msg.chat.id, msg.from.id+' - а это ваш id');
   writeWhoAsk(msg);
 })
 
