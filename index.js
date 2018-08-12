db488dd3ea90aad84e68f4864523f4e3fe6df369
Klimenko Ivan <kli-van@yandex.ru>
2018-08-13 01:02:37 +0300

    search-json parse

--- index.js
+++ index.js
@@ -60,26 +60,24 @@ function search(requestMes) {
   let term = requestMes;
 
   let response_handler = function (response) {
-      let body = '';
-      response.on('data', function (d) {
-          body += d;
-      });
-      response.on('end', function () {
-          console.log('\nRelevant Headers:\n');
-          for (var header in response.headers)
-              // header keys are lower-cased by Node.js
-              if (header.startsWith("bingapis-") || header.startsWith("x-msedge-"))
-                  console.log(header + ": " + response.headers[header]);
-          body = JSON.stringify(JSON.parse(body), null, '  ');
-          // console.log('\nJSON Response:\n');
-          // console.log(body);
-
-      });
-      response.on('error', function (e) {
-          console.log('Error: ' + e.message);
-      });
-
-      return body;
+    let body = '';
+    response.on('data', function (d) {
+        body += d;
+    });
+    response.on('end', function () {
+        console.log('\nRelevant Headers:\n');
+        for (var header in response.headers)
+            // header keys are lower-cased by Node.js
+            if (header.startsWith("bingapis-") || header.startsWith("x-msedge-"))
+                console.log(header + ": " + response.headers[header]);
+        body = JSON.stringify(JSON.parse(body), null, '  ');
+        // console.log('\nJSON Response:\n');
+        // console.log(body);
+        return body;
+    });
+    response.on('error', function (e) {
+        console.log('Error: ' + e.message);
+    });
   };
 
   let bing_image_search = function (search) {
