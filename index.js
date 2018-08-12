fcb0b1151a910a1cfc27b4457c6f41d7ad33c207
Klimenko Ivan <kli-van@yandex.ru>
2018-08-12 13:55:40 +0300

    roll

--- index.js
+++ index.js
@@ -210,8 +210,8 @@ bot.onText(/\/roll_interval ([0-9]+) ([0-9]+)/, (msg, match) => {
   bot.sendMessage(msg.chat.id, msg.from.first_name+' отладка min '+min)
   let max = match[2]
   bot.sendMessage(msg.chat.id, msg.from.first_name+' отладка max '+max)
-  let roll = (Math.random() * (max - min)) + min
-  bot.sendMessage(msg.chat.id, msg.from.first_name+' отладка '+roll)
+  let roll = (Math.random() * (max - min) + min)
+  bot.sendMessage(msg.chat.id, msg.from.first_name+' отладка roll '+roll)
   let roundRoll =  Math.round(roll)
   bot.sendMessage(msg.chat.id, msg.from.first_name+' выбросил '+roundRoll)
   
