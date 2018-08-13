f4561c710d1825064422607126559c0198a54c96
Klimenko Ivan <kli-van@yandex.ru>
2018-08-13 04:24:15 +0300

    reorganise some code

--- index.js
+++ index.js
@@ -258,7 +258,7 @@ bot.onText(/\/how_much_ero/, (msg) => {
   let array = null;
   let number = null;
   // открываем файл-буфер со ссылками
-  fs.readFile("./list/images.txt", "utf8", function(error,data) {
+  fs.readFile("./list/ero.txt", "utf8", function(error,data) {
     if(error) throw error; // если возникла ошибка
     // разбиваем содержимое файла на массив
     array = data.split(' ');
@@ -349,15 +349,65 @@ bot.onText(/\/roll_file ([0-9]+)/, (msg, match) => {
 
 // поиск картинки по запросу с выдачей первого результата
 bot.onText(/\/search (.+)/, (msg, match) => {
-  let text = match[1];
   bot.sendMessage(msg.chat.id, 'Пытаюсь найти '+text);
   // обнуляю файл после предыдущего запроса
   fs.writeFileSync("./list/search.txt", '', function(error){
     if(error) throw error; // если возникла ошибка
   });
-  search(text)
-  takeFromBuffer("./list/search.txt", msg.chat.id, true)
-  if (writeWhoAskFlag) writeWhoAsk(msg);
+  let subscriptionKey = azureKey();
+  let host = 'api.cognitive.microsoft.com';
+  let path = '/bing/v7.0/images/search';
+  let term = match[1];
+
+  let response_handler = function (response) {
+    let body = '';
+    response.on('data', function (d) {
+        body += d;
+    });
+    response.on('end', function () {
+      console.log('\nRelevant Headers:\n');
+      for (var header in response.headers)
+          // header keys are lower-cased by Node.js
+          if (header.startsWith("bingapis-") || header.startsWith("x-msedge-"))
+              console.log(header + ": " + response.headers[header]);
+      // в этом месте парсю тело запроса и вытаскиваю массив со свойствами (одно из них мне нужно чтобы скинуть картинку в чат)
+      let jsonAnswer = JSON.parse(body);
+      let valueArray = jsonAnswer.value;
+      // вытаскиваю ссылку на картинку в буфер
+      valueArray.forEach(function(item, i, valueArray) {
+        fs.appendFileSync("./list/search.txt", ' '+item.contentUrl, function(error){
+          if(error) throw error; // если возникла ошибка)
+        });
+      });
+    });
+    response.on('error', function (e) {
+      console.log('Error: ' + e.message);
+    });
+  };
+
+  let bing_image_search = function (search) {
+    console.log('Searching images for: ' + term);
+    let request_params = {
+          method : 'GET',
+          hostname : host,
+          path : path + '?q=' + encodeURIComponent(search),
+          headers : {
+              'Ocp-Apim-Subscription-Key' : subscriptionKey,
+          }
+    };
+
+    let req = https.request(request_params, response_handler);
+    req.end();
+
+    takeFromBuffer("./list/search.txt", msg.chat.id, true)
+  }
+
+  if (subscriptionKey.length === 32) {
+    bing_image_search(term);
+  } else {
+    console.log('Invalid Bing Search API subscription key!');
+    console.log('Please paste yours into the source code.');
+  }
 });
 
 // если нужно следующий результат
@@ -365,6 +415,5 @@ bot.onText(/\/more/, (msg) => {
   takeFromBuffer("./list/search.txt", msg.chat.id, true)
   if (writeWhoAskFlag) writeWhoAsk(msg);
 });
-
 
 // --- конец логики бота --- //
