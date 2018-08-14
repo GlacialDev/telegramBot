3e98be7d661a11969faf980c1486f59b79b4be37
Klimenko Ivan <kli-van@yandex.ru>
2018-08-14 04:10:04 +0300

    replacer

--- index.js
+++ index.js
@@ -169,17 +169,17 @@ function replacer(path1, path2, id) {
     if(error) throw error; // если возникла ошибка
     let string = data
     // вставить их в path2
-    fs.appendFileSync(path2, string+' ', function(error){
+    fs.appendFile(path2, string+' ', function(error){
       if(error) throw error; // если возникла ошибка)
-    })
-    // достать данные из path2 и отформатировать
-    fs.readFileSync(path2, "utf8", function(error,data){
-      if(error) throw error; // если возникла ошибка
-      let string2 = data.replace(/\s+/g, ' ')
-      // отформатированное записать обратно
-      fs.writeFile(path2, string2, function(error){
-        if(error) throw error; // если возникла ошибка)
-        bot.sendMessage(id, `Перемещение закончено`)
+      // достать данные из path2 и отформатировать
+      fs.readFile(path2, "utf8", function(error,data){
+        if(error) throw error; // если возникла ошибка
+        let string2 = data.replace(/\s+/g, ' ')
+        // отформатированное записать обратно
+        fs.writeFile(path2, string2, function(error){
+          if(error) throw error; // если возникла ошибка)
+          bot.sendMessage(id, `Перемещение закончено`)
+        })
       })
     })
   })
