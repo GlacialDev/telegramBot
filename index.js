f707ea77effc21da83a576639f44a6d106460a59
Klimenko Ivan <kli-van@yandex.ru>
2018-08-14 04:13:19 +0300

    replacer

--- index.js
+++ index.js
@@ -168,7 +168,7 @@ function replacer(path1, path2, id) {
   fs.readFile(path1, "utf8", function(error,data){
     if(error) throw error; // если возникла ошибка
     let string = data
-    // вставить их в path2
+    // вставить данные в path2
     fs.appendFile(path2, string+' ', function(error){
       if(error) throw error; // если возникла ошибка)
       // достать данные из path2 и отформатировать
@@ -181,6 +181,10 @@ function replacer(path1, path2, id) {
           bot.sendMessage(id, `Перемещение закончено`)
         })
       })
+    })
+    // очистить файл path1
+    fs.writeFile(path1, '', function(error){
+      if(error) throw error; // если возникла ошибка)
     })
   })
 }
