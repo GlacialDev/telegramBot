c9004687366fa1811f0a041ecd93bf1a0305c204
Klimenko Ivan <kli-van@yandex.ru>
2018-08-15 04:47:48 +0300

    help update

--- dont_needed_code_parts/code.js
+++ dont_needed_code_parts/code.js
@@ -25,23 +25,11 @@ bot.onText(/\/note/, (msg) => {
   bot.sendDocument(msg.chat.id, note);
 });
 
-// выкинуть случайное число от 0 до 100
-bot.onText(/(\/roll$)/, (msg) => {
+// выкинуть случайное число от 0 до 100 (другой способ)
+bot.onText(/\/random/, (msg) => {
   if (authCheck(msg) != true) return
-  let min = 0
-  let max = 100
-  let roll = Math.random() * (max - min) + min
-  let roundRoll =  Math.round(roll)
-  bot.sendMessage(msg.chat.id, msg.from.first_name+' выбросил '+roundRoll)
-});
 
-// выкинуть случайное число в заданном интервале
-bot.onText(/\/roll ([0-9]+) ([0-9]+)/, (msg, match) => {
-  if (authCheck(msg) != true) return
-  let min = +match[1]
-  let max = +match[2]
-  let roll = Math.random() * (max - min) + min
-  let roundRoll =  Math.round(roll)
+  let roundRoll =  Math.round(random(0,100))
   bot.sendMessage(msg.chat.id, msg.from.first_name+' выбросил '+roundRoll)
 });
 
