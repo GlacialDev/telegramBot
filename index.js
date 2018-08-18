17b7fc53b1abe64ed927d533dd64881d6e159793
Klimenko Ivan <kli-van@yandex.ru>
2018-08-19 02:40:10 +0300

    just check

--- index.js
+++ index.js
@@ -1,37 +1,41 @@
-// импорт и инициализация команд бота
-import help from './modules/commands/help'
-help()
-import echo from './modules/commands/echo'
-echo()
-import id from './modules/commands/id'
-id()
-import download from './modules/commands/download'
-download()
-import convert from './modules/commands/convert'
-convert()
-import ero_timer from './modules/commands/ero_timer'
-ero_timer()
-import pass_gen from './modules/commands/pass_gen'
-pass_gen()
-import photo from './modules/commands/photo'
-photo()
-import remind_me from './modules/commands/remind_me'
-remind_me()
-import roll from './modules/commands/roll'
-roll()
-import search from './modules/commands/search'
-search()
-import send_to from './modules/commands/send_to'
-send_to()
-import settings from './modules/commands/settings'
-settings()
-// в отличие от других команд, которые вызываются у бота одноименно, здесь нужно писать "!бот, (текст)"
-import talk_with_bot from './modules/commands/talk_with_bot'
-talk_with_bot()
+// // импорт и инициализация команд бота
+// import help from './modules/commands/help'
+// help()
+// import echo from './modules/commands/echo'
+// echo()
+// import id from './modules/commands/id'
+// id()
+// import download from './modules/commands/download'
+// download()
+// import convert from './modules/commands/convert'
+// convert()
+// import ero_timer from './modules/commands/ero_timer'
+// ero_timer()
+// import pass_gen from './modules/commands/pass_gen'
+// pass_gen()
+// import photo from './modules/commands/photo'
+// photo()
+// import remind_me from './modules/commands/remind_me'
+// remind_me()
+// import roll from './modules/commands/roll'
+// roll()
+// import search from './modules/commands/search'
+// search()
+// import send_to from './modules/commands/send_to'
+// send_to()
+// import settings from './modules/commands/settings'
+// settings()
+// // в отличие от других команд, которые вызываются у бота одноименно, здесь нужно писать "!бот, (текст)"
+// import talk_with_bot from './modules/commands/talk_with_bot'
+// talk_with_bot()
 
 
-// включение бота с запуском эро-таймера
+// // включение бота с запуском эро-таймера
+// import { bot, creator } from './variables'
+// bot.sendMessage(creator, `Бот инициализирован`)
+// import eroInit from './modules/functions/eroInit'
+// eroInit()
+
+
 import { bot, creator } from './variables'
-bot.sendMessage(creator, `Бот инициализирован`)
-import eroInit from './modules/functions/eroInit'
-eroInit()
+console.log( bot, creator )
