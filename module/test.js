a2a318862eab552d1e1274654dd6ffb2a8668439
Klimenko Ivan <kli-van@yandex.ru>
2018-08-18 23:15:29 +0300

    try modules again

--- module/test.js
+++ module/test.js
@@ -1,7 +1,8 @@
 export default function echo(telegramBot) {
+    console.log('meow')
     telegramBot.onText(/\/echo (.+)/, (msg, match) => {
         // if (authCheck(msg) != true) return
-      
+        console.log('meow')
         let text = match[1];
         bot.sendMessage(msg.chat.id, text);
       });
