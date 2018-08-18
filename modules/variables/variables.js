947e6aed9b1b0b8fa434f74a37913891feac0fff
Klimenko Ivan <kli-van@yandex.ru>
2018-08-19 01:50:59 +0300

    hope it works

--- modules/variables/variables.js
+++ modules/variables/variables.js
@@ -3,6 +3,7 @@ import config from '../../secret/config'
 export const creator = 353140575
 export const groupChat = -307924393
 export const https = require('https')
+const apiai = require('apiai')
 export const dialogflow = apiai(config.dialogFlowClientAccessToken);
 export const cloudconvert = new (require('cloudconvert'))(config.cloudConvertApiKey);
 export const fs = require('fs');
