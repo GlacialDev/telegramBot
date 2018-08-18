3656d7fd88fcde692911e4bd34a29fe76d34bab9
Klimenko Ivan <kli-van@yandex.ru>
2018-08-18 23:13:35 +0300

    try modules again

--- module/test.js
+++ module/test.js
@@ -1,6 +1,6 @@
 export default function echo(telegramBot) {
     telegramBot.onText(/\/echo (.+)/, (msg, match) => {
-        if (authCheck(msg) != true) return
+        // if (authCheck(msg) != true) return
       
         let text = match[1];
         bot.sendMessage(msg.chat.id, text);
