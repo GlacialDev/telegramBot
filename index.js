0a50bf21c02b3168990ae8c260236dc7152f84e0
Klimenko Ivan <kli-van@yandex.ru>
2018-08-20 02:34:36 +0300

    search

--- index.js
+++ index.js
@@ -432,21 +432,21 @@ bot.onText(/\/roll ([0-9]+) ([0-9]+)/, (msg, match) => {
 });
 
 // поиск картинки по запросу с выдачей первого результата
-bot.onText(/\/search (.+)/, (msg, match) => {
-  if (authCheck(msg) != true) return
+// bot.onText(/\/search (.+)/, (msg, match) => {
+//   if (authCheck(msg) != true) return
 
-  let text = match[1];
-  // обнуляю файл после предыдущего запроса
-  fs.writeFileSync("./list/search.txt", '', function (error) {
-    if (error) throw error; // если возникла ошибка
-  });
-  search(text)
+//   let text = match[1];
+//   // обнуляю файл после предыдущего запроса
+//   fs.writeFileSync("./list/search.txt", '', function (error) {
+//     if (error) throw error; // если возникла ошибка
+//   });
+//   search(text)
 
-  bot.sendMessage(msg.chat.id, 'Ищу ' + text + '. Результат ждите через 3 секунды');
-  setTimeout(function () {
-    takePhotoFromBuffer("./list/search.txt", msg.chat.id, false)
-  }, 3000);
-});
+//   bot.sendMessage(msg.chat.id, 'Ищу ' + text + '. Результат ждите через 3 секунды');
+//   setTimeout(function () {
+//     takePhotoFromBuffer("./list/search.txt", msg.chat.id, false)
+//   }, 3000);
+// });
 
 // если нужно следующий результат
 bot.onText(/\/search_more/, (msg) => {
