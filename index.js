fdb3415e1d087153e642e79c0de511263ddede59
Klimenko Ivan <kli-van@yandex.ru>
2018-08-21 01:40:29 +0300

    remind_me

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ import id from './modules/bot_commands/id'
 id()
 import remind_me from './modules/bot_commands/remind_me'
 remind_me()
+import roll from './modules/bot_commands/roll'
+roll()
---
+++ modules/bot_commands/roll.js
@@ -0,0 +1,30 @@
+import variables from '../variables/variables'
+import authCheck from '../functions/authCheck'
+
+let bot = variables.bot
+
+export default function roll() {
+// выкинуть случайное число от 0 до 100
+    bot.onText(/(\/roll$)/, (msg) => {
+        if (authCheck(msg) != true) return
+    
+        let min = 0
+        let max = 100
+        let roll = Math.random() * (max - min) + min
+        let roundRoll = Math.round(roll)
+    
+        bot.sendMessage(msg.chat.id, msg.from.first_name + ' выбросил ' + roundRoll)
+    });
+    
+    // выкинуть случайное число в заданном интервале
+    bot.onText(/\/roll ([0-9]+) ([0-9]+)/, (msg, match) => {
+        if (authCheck(msg) != true) return
+        
+        let min = +match[1]
+        let max = +match[2]
+        let roll = Math.random() * (max - min) + min
+        let roundRoll = Math.round(roll)
+    
+        bot.sendMessage(msg.chat.id, msg.from.first_name + ' выбросил ' + roundRoll)
+    });
+}
