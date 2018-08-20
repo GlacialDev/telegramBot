f31deb9b843e15ef58b6de587ee6e87c97dd9cc3
Klimenko Ivan <kli-van@yandex.ru>
2018-08-21 02:09:00 +0300

    forget to enable it

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ import roll from './modules/bot_commands/roll'
 roll()
 import talk_with_bot from './modules/bot_commands/talk_with_bot'
 talk_with_bot()
+import settings from './modules/bot_commands/settings'
+settings()
+import set_ero_timer from './modules/bot_commands/set_ero_timer'
+set_ero_timer()
+import stop_ero_timer from './modules/bot_commands/stop_ero_timer'
+stop_ero_timer()
+import how_much_timer from './modules/bot_commands/how_much_ero'
+how_much_ero()
