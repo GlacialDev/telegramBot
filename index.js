e15157d2ee488a1b819d9cc187d6ed9a34add49a
Klimenko Ivan <kli-van@yandex.ru>
2018-08-21 02:25:45 +0300

    settings

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ import roll from './modules/bot_commands/roll'
 roll()
 import talk_with_bot from './modules/bot_commands/talk_with_bot'
 talk_with_bot()
-import bot_settings from './modules/bot_commands/settings'
-bot_settings()
 import set_ero_timer from './modules/bot_commands/set_ero_timer'
 set_ero_timer()
 import stop_ero_timer from './modules/bot_commands/stop_ero_timer'
@@ -18,3 +16,5 @@ import how_much_ero from './modules/bot_commands/how_much_ero'
 how_much_ero()
 import give_ero from './modules/bot_commands/give_ero'
 give_ero()
+import bot_settings from './modules/bot_commands/settings'
+bot_settings()
--- modules/bot_commands/settings.js
+++ modules/bot_commands/settings.js
@@ -3,13 +3,14 @@ import settings from '../variables/settings'
 import authCheck from '../functions/authCheck'
 
 let bot = variables.bot
-let eroInterval = settings.eroInterval
-let eroTimerStateFlag = settings.eroTimerStateFlag
-let downloadEnabledFlag = settings.downloadEnabledFlag
 
 export default function bot_settings() {
     bot.onText(/\/settings/, (msg) => {
         if (authCheck(msg) != true) return
+
+        let eroInterval = settings.eroInterval
+        let eroTimerStateFlag = settings.eroTimerStateFlag
+        let downloadEnabledFlag = settings.downloadEnabledFlag
 
         bot.sendMessage(msg.chat.id,
 `Настройки:
