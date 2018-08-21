e8acae60705afb010603538f76ccb2b07e5d38a8
Klimenko Ivan <kli-van@yandex.ru>
2018-08-21 03:29:51 +0300

    explore objects

--- modules/bot_commands/how_much_ero.js
+++ modules/bot_commands/how_much_ero.js
@@ -10,10 +10,10 @@ export default function how_much_ero() {
     bot.onText(/\/how_much_ero/, (msg) => {
         if (authCheck(msg) != true) return
 
-        let number = eroTimerObj.how_much_ero()
+        let number = eroTimerObj.how_much_ero(msg)
         // bot.sendMessage(msg.chat.id, number)
 
-        
+
         // let array = null;
         // let number = null;
         // // открываем файл-буфер со ссылками
