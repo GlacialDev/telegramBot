d773af62fb4d181fe6d797ba78a00ed52c7e68e2
Klimenko Ivan <kli-van@yandex.ru>
2018-08-21 03:55:20 +0300

    explore objects

--- modules/objects/eroTimer.js
+++ modules/objects/eroTimer.js
@@ -7,11 +7,11 @@ let fs = variables.fs
 let groupChat = variables.groupChat
 
 let eroTimerObj = {
-    eroTimer : null,
-    eroInterval : 3600000*3,
-    eroTimerStateFlag : 'disabled',
+    eroTimer: null,
+    eroInterval: 3600000 * 3,
+    eroTimerStateFlag: 'disabled',
 
-    how_much_ero : (msg) => {
+    how_much_ero: (msg) => {
         let array = null;
         let number = null;
         // открываем файл-буфер со ссылками
@@ -24,7 +24,7 @@ let eroTimerObj = {
             bot.sendMessage(msg.chat.id, `У меня в запасе осталось ${number} картинок`)
         });
     },
-    set_ero_timer : (msg, match) => {
+    set_ero_timer: (msg, match) => {
         let hours = match[1]
         // чтобы картинки не улетали как бешенные :)
         if (hours < 1) {
@@ -34,7 +34,7 @@ let eroTimerObj = {
         // если переназначаем таймер, прошлый нужно остановить
         stopTimer(eroTimerObj.eroTimer)
         // значение интервала для таймера
-        eroTimerObj.eroInterval = 3600000*hours
+        eroTimerObj.eroInterval = 3600000 * hours
         eroTimerObj.eroTimerStateFlag = 'enabled'
         // инициализация таймера
         eroTimerObj.eroTimer = setTimeout(() => {
@@ -43,11 +43,36 @@ let eroTimerObj = {
         // если всё прошло успешно и без ошибок, далее следует сообщение в группу
         bot.sendMessage(groupChat, 'Буду присылать картинки каждые ' + hours + ' часов')
     },
-    stop_ero_timer : (msg) => {
+    stop_ero_timer: (msg) => {
         stopTimer(eroTimerObj.eroTimer)
         eroTimerObj.eroTimerStateFlag = 'disabled'
         // при остановке таймера группа об этом оповещается
         bot.sendMessage(groupChat, 'Таймер картинок остановлен')
+    },
+    add_more_ero: (msg) => {
+        // достать данные из './data/download/savefrom.txt'
+        fs.readFile('./data/download/savefrom.txt', "utf8", function (error, data) {
+            if (error) throw error; // если возникла ошибка
+            let string = data
+            // вставить данные в './data/eroTimer/ero.txt'
+            fs.appendFile('./data/eroTimer/ero.txt', string + ' ', function (error) {
+                if (error) throw error; // если возникла ошибка)
+                // достать данные из './data/eroTimer/ero.txt' и отформатировать
+                fs.readFile('./data/eroTimer/ero.txt', "utf8", function (error, data) {
+                    if (error) throw error; // если возникла ошибка
+                    let string2 = data.replace(/\s+/g, ' ')
+                    // отформатированное записать обратно
+                    fs.writeFile('./data/eroTimer/ero.txt', string2, function (error) {
+                        if (error) throw error; // если возникла ошибка
+                        bot.sendMessage(msg.chat.id, `Перемещение закончено`)
+                    })
+                })
+            })
+            // очистить файл './data/download/savefrom.txt'
+            fs.writeFile('./data/download/savefrom.txt', '', function (error) {
+                if (error) throw error; // если возникла ошибка
+            })
+        })
     }
 }
 
