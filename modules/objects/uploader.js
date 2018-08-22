1e3734557c9bffc024af66d9964f7ecfe3bf7384
Klimenko Ivan <kli-van@yandex.ru>
2018-08-23 00:12:52 +0300

    converter good

--- modules/objects/uploader.js
+++ modules/objects/uploader.js
@@ -66,13 +66,6 @@ let uploader = {
         .pipe(fs.createWriteStream('./data/converted/' + outputFileName))
         .on('finish', function () {
             bot.sendDocument(msg.chat.id, './data/converted/' + outputFileName)
-            // и удалить эти файлы потом
-            fs.unlink('./data/converted/' + outputFileName, (error) => {
-                if (error) throw error; // если возникла ошибка
-            })
-            fs.unlink('./data/download/' + inputfileName, (error) => {
-                if (error) throw error; // если возникла ошибка
-            })
             inputfileName = ''
         })
         .on('error', function () {
