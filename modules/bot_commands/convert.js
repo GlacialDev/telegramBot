3bfe2515ad85d415ee271d84b45c515822c146a5
Klimenko Ivan <kli-van@yandex.ru>
2018-08-23 00:07:15 +0300

    converter good

--- modules/bot_commands/convert.js
+++ modules/bot_commands/convert.js
@@ -3,16 +3,15 @@ import authCheck from '../functions/authCheck'
 import uploader from '../objects/uploader'
 
 let bot = variables.bot
-let U = new uploader
 
 export default function convert() {
     bot.onText(/\/convert to (.+)/, (msg, match) => {
         if (authCheck(msg) != true) return
 
-        U.upload(msg).then(
+        uploader.upload(msg).then(
             () => {
                 bot.sendMessage(msg.chat.id, 'Файл успешно загружен.')
-                U.convert(msg, match)
+                uploader.convert(msg, match)
             },
             () => bot.sendMessage(msg.chat.id, 'Файл не загрузился, какая-то ошибка.')
         )
