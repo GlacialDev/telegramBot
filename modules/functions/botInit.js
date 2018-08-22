915fb671cdf5ccb3eb3e24e6b5170f7384b4826b
Klimenko Ivan <kli-van@yandex.ru>
2018-08-23 00:46:30 +0300

    converter good

--- modules/functions/botInit.js
+++ modules/functions/botInit.js
@@ -1,5 +1,5 @@
-import variables from './modules/variables/variables'
-import eroTimerObj from './modules/objects/eroTimer'
+import variables from '../variables/variables'
+import eroTimerObj from '../objects/eroTimer'
 
 export default function botInit(flag) {
     variables.bot.sendMessage(variables.creator, `Бот инициализирован. dev-mode: ${flag}`)
