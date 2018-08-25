078c7d98ea2f74f7aa65abf999f8fa6c428397ad
Klimenko Ivan <kli-van@yandex.ru>
2018-08-25 21:28:26 +0300

    poll

--- modules/functions/botInit.js
+++ modules/functions/botInit.js
@@ -2,8 +2,10 @@ import variables from '../variables/variables'
 import eroTimerObj from '../objects/eroTimer'
 import pollManager from '../objects/pollManager'
 
+let bot = variables.bot
+
 export default function botInit(flag) {
-    variables.bot.sendMessage(variables.creator, `Бот инициализирован. dev-mode: ${flag}`)
+    bot.sendMessage(variables.creator, `Бот инициализирован. dev-mode: ${flag}`)
     eroTimerObj.eroTimerInit(flag)
 
     bot.on('callback_query', function (msg) {
