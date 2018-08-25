613e9a9d2f0ecbeca512b2fd75da42c80acbdca0
Klimenko Ivan <kli-van@yandex.ru>
2018-08-25 21:25:04 +0300

    poll

--- modules/bot_commands/make_poll.js
+++ modules/bot_commands/make_poll.js
@@ -7,39 +7,14 @@ import pollManager from '../objects/pollManager'
 let bot = variables.bot
 
 export default function make_poll() {
-    // let pollStore = []
-
     bot.onText(/\/make_poll (.+) - ответы - (.+)/, (msg, match) => {
         if (authCheck(msg) != true) return
 
-        let title = match[1]
-        let answers = match[2].split('/')
-        let id = symbolStringGenerator(15)
-
-        // let pollObject = new poll(title, answers, id)
-        // pollStore.push([id, pollObject])
-
-        // pollObject.make_poll(msg)
-
-        pollManager.createPoll(title, answers, id, msg)
+        pollManager.createPoll(msg, match)
     })
 
     bot.on('callback_query', function (msg) {
         let data = msg.data.split('_')
-        if(data[0] == 'poll') {
-            let id = data[1]
-            let answerNumber = data[2]
-            let clickedPoll
-    
-            for (let i = 0; i < pollStore.length; i++) {
-                if (pollStore[i][0] == id) {
-                    clickedPoll = pollStore[i][1]
-                    break
-                }
-            }
-            
-            clickedPoll.votes[answerNumber]++
-            clickedPoll.update_poll(msg)
-        }
+        if(data[0] == 'poll') pollManager.updatePoll(msg, data)
     })
 }
--- modules/objects/pollManager.js
+++ modules/objects/pollManager.js
@@ -1,16 +1,36 @@
 import variables from '../variables/variables';
 import poll from './poll'
+import symbolStringGenerator from '../functions/symbolStringGenerator'
 
 let bot = variables.bot
 
 let pollManager = {
     store: [],
     
-    createPoll: (title, answers, id, msg) => {
+    createPoll: (msg, match) => {
+        let title = match[1]
+        let answers = match[2].split('/')
+        let id = symbolStringGenerator(15)
+
         let pollObject = new poll(title, answers, id)
         pollManager.store.push([id, pollObject])
 
         pollObject.make_poll(msg)
+    },
+    updatePoll: (msg, data) => {
+        let id = data[1]
+        let answerNumber = data[2]
+        let clickedPoll
+
+        for (let i = 0; i < pollManager.store.length; i++) {
+            if (pollManager.store[i][0] == id) {
+                clickedPoll = pollManager.store[i][1]
+                break
+            }
+        }
+        
+        clickedPoll.votes[answerNumber]++
+        clickedPoll.update_poll(msg)
     }
 }
 
