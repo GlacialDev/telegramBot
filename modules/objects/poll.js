85354e93ae9274307b92071aed37370c3077424a
Klimenko Ivan <kli-van@yandex.ru>
2018-08-25 19:41:11 +0300

    poll

--- modules/objects/poll.js
+++ modules/objects/poll.js
@@ -20,7 +20,7 @@ class poll {
     make_poll(msg) {
         let options = {
             reply_markup: JSON.stringify({
-                inline_keyboard: buttons,
+                inline_keyboard: this.buttons,
                 parse_mode: 'Markdown'
             })
         }
