b442aa835a27aa9f06a7c99f69c39d19a126b6d3
Klimenko Ivan <kli-van@yandex.ru>
2018-08-25 04:37:54 +0300

    poll

--- modules/bot_commands/make_poll.js
+++ modules/bot_commands/make_poll.js
@@ -4,59 +4,13 @@ import authCheck from '../functions/authCheck'
 let bot = variables.bot
 
 export default function make_poll() {
-    bot.onText(/\/make_poll (.+) - ответы (.+)/, (msg, match) => {
+    bot.onText(/\/make_poll (.+) - ответы - (.+)/, (msg, match) => {
         if (authCheck(msg) != true) return
 
         let question = match[1]
         let answers = match[2].split('/')
-        let poll = {
-            title: question,
-            buttons: [],
-            votes: []
-        }
-        for (let i = 0; i < answers.length; i++) {
-            poll.votes[i] = 0
-            let objectBlanc = {
-                text : `${answers[i]} - ${poll.votes[i]}`,
-                callback_data : i
-            }
-            poll.buttons[i] = [objectBlanc]
-        }
-        let options = {
-            reply_markup: JSON.stringify({
-                inline_keyboard: poll.buttons,
-                parse_mode: 'Markdown'
-            })
-        };
 
-        bot.sendMessage(msg.chat.id, poll.title, options)
 
-        bot.on('callback_query', function (msg) {
-            let i = msg.data
-            let messageId = msg.message.message_id
-            let chatId = msg.message.chat.id
-            
-            poll.votes[i] = poll.votes[i]+1
-
-            for (let i = 0; i < answers.length; i++) {
-                let objectBlanc = {
-                    text : `${answers[i]} - ${poll.votes[i]}`,
-                    callback_data : i
-                }
-                poll.buttons[i] = [objectBlanc]
-            }
-            options = {
-                reply_markup: JSON.stringify({
-                    inline_keyboard: poll.buttons
-                })
-            };
-
-            bot.editMessageText(poll.title, {
-                message_id: messageId,
-                chat_id: chatId,
-                parse_mode: 'Markdown',
-                reply_markup: options.reply_markup
-            })
-          });
+        new poll(question, answers).make_poll()
     })
 }
---
+++ modules/objects/poll.js
@@ -0,0 +1,69 @@
+import variables from '../variables/variables'
+
+let bot = variables.bot
+
+let poll = {
+    question: '',
+    answers: [],
+    poll: {
+        title: question,
+        buttons: [],
+        votes: []
+    },
+    constructor(question, answers) {
+        this.question = question
+        this.answers = answers
+    }, 
+    make_poll() {
+        let poll = poll.poll
+        let question = poll.question
+        let answers = poll.answers
+
+        for (let i = 0; i < answers.length; i++) {
+            poll.votes[i] = 0
+            let objectBlanc = {
+                text: `${answers[i]} - ${poll.votes[i]}`,
+                callback_data: i
+            }
+            poll.buttons[i] = [objectBlanc]
+        }
+        let options = {
+            reply_markup: JSON.stringify({
+                inline_keyboard: poll.buttons,
+                parse_mode: 'Markdown'
+            })
+        };
+
+        bot.sendMessage(msg.chat.id, poll.title, options)
+
+        bot.on('callback_query', function (msg) {
+            let i = msg.data
+            let messageId = msg.message.message_id
+            let chatId = msg.message.chat.id
+
+            poll.votes[i] = poll.votes[i] + 1
+
+            for (let i = 0; i < answers.length; i++) {
+                let objectBlanc = {
+                    text: `${answers[i]} - ${poll.votes[i]}`,
+                    callback_data: i
+                }
+                poll.buttons[i] = [objectBlanc]
+            }
+            options = {
+                reply_markup: JSON.stringify({
+                    inline_keyboard: poll.buttons
+                })
+            };
+
+            bot.editMessageText(poll.title, {
+                message_id: messageId,
+                chat_id: chatId,
+                parse_mode: 'Markdown',
+                reply_markup: options.reply_markup
+            })
+        });
+    }
+}
+
+export default poll
