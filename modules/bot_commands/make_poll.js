760b0f493b3383cef3d4af4972cd0013c99ac962
Klimenko Ivan <kli-van@yandex.ru>
2018-08-25 18:34:38 +0300

    poll

--- modules/bot_commands/make_poll.js
+++ modules/bot_commands/make_poll.js
@@ -6,14 +6,24 @@ import poll from '../objects/poll'
 let bot = variables.bot
 
 export default function make_poll() {
+    let pollStore = []
+
     bot.onText(/\/make_poll (.+) - ответы - (.+)/, (msg, match) => {
         if (authCheck(msg) != true) return
 
         let question = match[1]
         let answers = match[2].split('/')
         let id = symbolStringGenerator(15)
-        console.log(id)
+        let poll = new poll(question, answers, id)
 
-        new poll(question, answers, id).make_poll(msg)
+        pollStore.push([id, poll])
+
+        poll.make_poll(msg)
+    })
+
+    bot.on('callback_query', function (msg) {
+        let data = msg.data.split('_')
+        let id = data[0]
+        let answerNumber = data[1]
     })
 }
--- modules/objects/poll.js
+++ modules/objects/poll.js
@@ -5,30 +5,28 @@ let bot = variables.bot
 class poll {
     title
     answers
-    pollId
+    id
+    buttons = []
+    votes = []
 
-    constructor(title, answers, pollId) {
+    constructor(title, answers, id) {
         poll.title = title
         poll.answers = answers
-        poll.pollId = pollId
-
-        console.log(pollId+' pollId inside constructor')
+        poll.id = id
     }
 
     make_poll(msg) {
         let title = poll.title
         let answers = poll.answers
-        let pollId = poll.pollId
-        let buttons = []
-        let votes = []
+        let id = poll.id
+        let buttons = poll.buttons
+        let votes = poll.votes
 
-        console.log(pollId+' pollId inside make_poll')
-        
         for (let i = 0; i < answers.length; i++) {
             votes[i] = 0
             let objectBlanc = {
                 text: `${answers[i]} - ${votes[i]}`,
-                callback_data: pollId+'_'+i
+                callback_data: id+'_'+i
             }
             buttons[i] = [objectBlanc]
         }
@@ -40,40 +38,34 @@ class poll {
         };
 
         bot.sendMessage(msg.chat.id, title, options)
-        
-        bot.on('callback_query', function (msg) {
-            let data = msg.data.split('_')
-            console.log(data)
-            let pollId = data[0]
-            console.log(pollId)
+    }
 
-            if (pollId != poll.pollId) return
-            let i = data[1]
-            let messageId = msg.message.message_id
-            let chatId = msg.message.chat.id
+    update_poll(msg) {
+        let messageId = msg.message.message_id
+        let chatId = msg.message.chat.id
 
-            votes[i] = votes[i] + 1
+        votes[i] = votes[i] + 1
 
-            for (let i = 0; i < answers.length; i++) {
-                let objectBlanc = {
-                    text: `${answers[i]} - ${votes[i]}`,
-                    callback_data: pollId+'_'+i
-                }
-                buttons[i] = [objectBlanc]
+        for (let i = 0; i < answers.length; i++) {
+            let objectBlanc = {
+                text: `${answers[i]} - ${votes[i]}`,
+                callback_data: pollId+'_'+i
             }
-            options = {
-                reply_markup: JSON.stringify({
-                    inline_keyboard: buttons
-                })
-            };
-
-            bot.editMessageText(title, {
-                message_id: messageId,
-                chat_id: chatId,
-                parse_mode: 'Markdown',
-                reply_markup: options.reply_markup
+            buttons[i] = [objectBlanc]
+        }
+        options = {
+            reply_markup: JSON.stringify({
+                inline_keyboard: buttons
             })
-        });
+        };
+
+        bot.editMessageText(title, {
+            message_id: messageId,
+            chat_id: chatId,
+            parse_mode: 'Markdown',
+            reply_markup: options.reply_markup
+        })
+    });
     }
 }
 
