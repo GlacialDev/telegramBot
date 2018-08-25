43966cedab7390f4201ac23871133553cb490957
Klimenko Ivan <kli-van@yandex.ru>
2018-08-25 03:25:50 +0300

    poll

--- modules/bot_commands/make_poll.js
+++ modules/bot_commands/make_poll.js
@@ -7,6 +7,8 @@ export default function make_poll() {
     bot.onText(/\/make_poll (.+) answers (.+)/, (msg, match) => {
         if (authCheck(msg) != true) return
 
+        let chatId = msg.chat.id
+
         let question = match[1]
         let answers = match[2].split('/')
         let poll = {
@@ -39,23 +41,21 @@ export default function make_poll() {
             poll.votes[i] = poll.votes[i]+1
             console.log(poll.votes[i]+' posle')
 
-            console.log(poll)
+            for (let i = 0; i < answers.length; i++) {
+                let objectBlanc = {
+                    text : `${answers[i]} - ${poll.votes[i]}`,
+                    callback_data : i
+                }
+                poll.buttons[i] = [objectBlanc]
+            }
+            options = {
+                reply_markup: JSON.stringify({
+                    inline_keyboard: poll.buttons,
+                    parse_mode: 'Markdown'
+                })
+            };
 
-            // for (let i = 0; i < answers.length; i++) {
-            //     let objectBlanc = {
-            //         text : `${answers[i]} - ${poll.votes[i]}`,
-            //         callback_data : i
-            //     }
-            //     poll.buttons[i] = [objectBlanc]
-            // }
-            // options = {
-            //     reply_markup: JSON.stringify({
-            //         inline_keyboard: poll.buttons,
-            //         parse_mode: 'Markdown'
-            //     })
-            // };
-
-            // bot.editMessageReplyMarkup(chatId, messageId, options.reply_markup)
+            bot.editMessageReplyMarkup(options.reply_markup)
           });
     })
 }
