7e647d2b65c40e6c89cef115bc218cf8c59edcb1
Klimenko Ivan <kli-van@yandex.ru>
2018-08-25 21:19:05 +0300

    poll

--- modules/objects/pollManager.js
+++ modules/objects/pollManager.js
@@ -4,9 +4,9 @@ import poll from './poll'
 let bot = variables.bot
 
 let pollManager = {
-    store = [],
+    store: [],
     
-    createPoll(title, answers, id, msg) {
+    createPoll: (title, answers, id, msg) => {
         let pollObject = new poll(title, answers, id)
         this.store.push([id, pollObject])
 
