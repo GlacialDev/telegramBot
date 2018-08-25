ae1a6189db2f9053b1512a19a1abce6b291d08bf
Klimenko Ivan <kli-van@yandex.ru>
2018-08-25 05:07:17 +0300

    poll

--- modules/objects/poll.js
+++ modules/objects/poll.js
@@ -17,52 +17,52 @@ class poll {
         console.log('makepoll after let')
         
         console.log(title, answers, buttons, votes)
-        // for (let i = 0; i < answers.length; i++) {
-        //     votes[i] = 0
-        //     let objectBlanc = {
-        //         text: `${answers[i]} - ${votes[i]}`,
-        //         callback_data: i
-        //     }
-        //     buttons[i] = [objectBlanc]
-        // }
-        // let options = {
-        //     reply_markup: JSON.stringify({
-        //         inline_keyboard: buttons,
-        //         parse_mode: 'Markdown'
-        //     })
-        // };
+        for (let i = 0; i < answers.length; i++) {
+            votes[i] = 0
+            let objectBlanc = {
+                text: `${answers[i]} - ${votes[i]}`,
+                callback_data: i
+            }
+            buttons[i] = [objectBlanc]
+        }
+        let options = {
+            reply_markup: JSON.stringify({
+                inline_keyboard: buttons,
+                parse_mode: 'Markdown'
+            })
+        };
 
-        // console.log('makepoll after options')
-        // bot.sendMessage(msg.chat.id, title, options)
+        console.log('makepoll after options')
+        bot.sendMessage(msg.chat.id, title, options)
 
-        // console.log('makepoll after sendmessage')
-        // bot.on('callback_query', function (msg) {
-        //     let i = msg.data
-        //     let messageId = msg.message.message_id
-        //     let chatId = msg.message.chat.id
+        console.log('makepoll after sendmessage')
+        bot.on('callback_query', function (msg) {
+            let i = msg.data
+            let messageId = msg.message.message_id
+            let chatId = msg.message.chat.id
 
-        //     votes[i] = votes[i] + 1
+            votes[i] = votes[i] + 1
 
-        //     for (let i = 0; i < answers.length; i++) {
-        //         let objectBlanc = {
-        //             text: `${answers[i]} - ${votes[i]}`,
-        //             callback_data: i
-        //         }
-        //         buttons[i] = [objectBlanc]
-        //     }
-        //     options = {
-        //         reply_markup: JSON.stringify({
-        //             inline_keyboard: buttons
-        //         })
-        //     };
+            for (let i = 0; i < answers.length; i++) {
+                let objectBlanc = {
+                    text: `${answers[i]} - ${votes[i]}`,
+                    callback_data: i
+                }
+                buttons[i] = [objectBlanc]
+            }
+            options = {
+                reply_markup: JSON.stringify({
+                    inline_keyboard: buttons
+                })
+            };
 
-        //     bot.editMessageText(title, {
-        //         message_id: messageId,
-        //         chat_id: chatId,
-        //         parse_mode: 'Markdown',
-        //         reply_markup: options.reply_markup
-        //     })
-        // });
+            bot.editMessageText(title, {
+                message_id: messageId,
+                chat_id: chatId,
+                parse_mode: 'Markdown',
+                reply_markup: options.reply_markup
+            })
+        });
     }
 }
 
