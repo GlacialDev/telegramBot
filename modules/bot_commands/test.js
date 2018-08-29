cb001afafd3ab57ea57d78fb8c88a54bfb923a4f
Klimenko Ivan <kli-van@yandex.ru>
2018-08-30 00:32:38 +0300

    reaction

--- modules/bot_commands/test.js
+++ modules/bot_commands/test.js
@@ -1,6 +1,7 @@
 import variables from '../variables/variables'
 import authCheck from '../functions/authCheck'
 import pollManager from '../objects/pollManager'
+import config from '../../secret/config'
 
 let bot = variables.bot
 
@@ -8,6 +9,6 @@ export default function test() {
     bot.onText(/\/test/, (msg, match) => {
         if (authCheck(msg) != true) return
         
-        pollManager.createReaction(msg, 'Оцените девочку')
+        pollManager.createReaction(msg, 'Оцените девочку', config.canadianEroId)
     });
 } 
