054eae29d4bb35d701cfc590661d4198338c9008
Klimenko Ivan <kli-van@yandex.ru>
2018-08-30 01:34:31 +0300

    reaction

--- modules/functions/getEroPhotoLink.js
+++ modules/functions/getEroPhotoLink.js
@@ -3,19 +3,23 @@ import variables from '../variables/variables'
 let fs = variables.fs
 
 export default function getEroPhotoLink(path) {
+    let array
+    let link
+    let string
     // открываем файл-буфер со ссылками
-    fs.readFile(path, "utf8", function (error, data) {
+    fs.readFileSync(path, "utf8", function (error, data) {
       if (error) throw error; // если возникла ошибка
       // разбиваем содержимое файла на массив и достаем оттуда одну ссылку
-      let array = data.split(' ');
-      let link = array.shift();
+      array = data.split(' ');
+      link = array.shift();
       // массив без элемента который мы достали shift-ом преобразуем в строку
-      let string = array.join(' ')
+      string = array.join(' ')
       // и грузим обратно в файл-буфер
       fs.writeFile(path, string, function (error) {
         if (error) throw error; // если возникла ошибка)
       });
-      return new Promise((resolve, reject) => {
+    });
+    return new Promise((resolve, reject) => {
         if (link == '') {
             console.log('in reject')
             reject()
@@ -24,5 +28,4 @@ export default function getEroPhotoLink(path) {
             resolve(link)
         }
       })
-    });
   }
