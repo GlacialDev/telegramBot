c3810a61dbcf7154567fefacc726b17eac87cf53
Klimenko Ivan <kli-van@yandex.ru>
2018-08-31 17:33:19 +0300

    database

--- modules/objects/pollManager.js
+++ modules/objects/pollManager.js
@@ -53,54 +53,52 @@ let pollManager = {
 
         console.log(clickedPoll)
         console.log(userVotes)
-        
-        break;
-        
-        // for (let i = 0; i < pollManager.pollStore.length; i++) {
-        //     if (pollManager.pollStore[i][0] == id) {
-        //         clickedPoll = pollManager.pollStore[i][1]
-        //         userVotes = pollManager.pollStore[i][2]
-        //         break
+
+        // // for (let i = 0; i < pollManager.pollStore.length; i++) {
+        // //     if (pollManager.pollStore[i][0] == id) {
+        // //         clickedPoll = pollManager.pollStore[i][1]
+        // //         userVotes = pollManager.pollStore[i][2]
+        // //         break
+        // //     }
+        // // }
+        // // если в списке проголосовавших человек уже есть
+        // if(userVotes[0].includes(userId)) {
+        //     let userPos = userVotes[0].indexOf(userId) // на той же позиции всегда находится и номер ответа
+        //     let lastAnswerNumber = userVotes[1][userPos]
+        //     // если он кликнул туда же, куда и в прошлый раз, убрать его голос
+        //     if(lastAnswerNumber == answerNumber) {
+        //         userVotes[0].splice(userPos, 1)
+        //         userVotes[1].splice(userPos, 1)
+        //         clickedPoll.votes[lastAnswerNumber]--
+        //     // если он кликнул в другой вариант, перезаписать результат голосования
+        //     } else {
+        //         userVotes[0].splice(userPos, 1)
+        //         userVotes[1].splice(userPos, 1)
+        //         clickedPoll.votes[lastAnswerNumber]--
+        //         userVotes[0].push(userId)
+        //         userVotes[1].push(answerNumber)
+        //         clickedPoll.votes[answerNumber]++
         //     }
+        // // если в списке голосовавших его не было, добавить
+        // } else {
+        //     userVotes[0].push(userId)
+        //     userVotes[1].push(answerNumber)
+        //     clickedPoll.votes[answerNumber]++
         // }
-        // если в списке проголосовавших человек уже есть
-        if(userVotes[0].includes(userId)) {
-            let userPos = userVotes[0].indexOf(userId) // на той же позиции всегда находится и номер ответа
-            let lastAnswerNumber = userVotes[1][userPos]
-            // если он кликнул туда же, куда и в прошлый раз, убрать его голос
-            if(lastAnswerNumber == answerNumber) {
-                userVotes[0].splice(userPos, 1)
-                userVotes[1].splice(userPos, 1)
-                clickedPoll.votes[lastAnswerNumber]--
-            // если он кликнул в другой вариант, перезаписать результат голосования
-            } else {
-                userVotes[0].splice(userPos, 1)
-                userVotes[1].splice(userPos, 1)
-                clickedPoll.votes[lastAnswerNumber]--
-                userVotes[0].push(userId)
-                userVotes[1].push(answerNumber)
-                clickedPoll.votes[answerNumber]++
-            }
-        // если в списке голосовавших его не было, добавить
-        } else {
-            userVotes[0].push(userId)
-            userVotes[1].push(answerNumber)
-            clickedPoll.votes[answerNumber]++
-        }
         
-        clickedPoll.update_poll(msg)
+        // clickedPoll.update_poll(msg)
 
-        let options_put = {
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                poll: clickedPoll,
-                userVotes : userVotes
-            })
-        }
+        // let options_put = {
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //     },
+        //     body: JSON.stringify({
+        //         poll: clickedPoll,
+        //         userVotes : userVotes
+        //     })
+        // }
 
-        request.put('http://localhost:3012/pollstore', options_put);
+        // request.put('http://localhost:3012/pollstore', options_put);
     },
     createReaction: (link, sendTo) => {
         let id = symbolStringGenerator(16)
