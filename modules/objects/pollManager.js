f27de7e05d743b2fb7264be38103ec5910db482f
Klimenko Ivan <kli-van@yandex.ru>
2018-08-31 17:13:23 +0300

    database

--- modules/objects/pollManager.js
+++ modules/objects/pollManager.js
@@ -43,7 +43,7 @@ let pollManager = {
         let getReqResult
         
         console.log('v update')
-        requestP.get('http://localhost:3012/pollstore/'+id).then((poll) => {console.log(typeof poll)})
+        requestP.get('http://localhost:3012/pollstore/'+id).then((poll) => {console.log(toObject(poll))})
 
     //     // ищем нужный опрос (потому что их может работать одновременно несколько) по id опроса
     //     // и запоминаем его, а также кто за что в нем голосовал (userVotes)
