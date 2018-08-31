2faf273455f53c4ad8d1740998805912ada0264e
Klimenko Ivan <kli-van@yandex.ru>
2018-09-01 00:22:54 +0300

    database

--- modules/objects/pollManager.js
+++ modules/objects/pollManager.js
@@ -1,10 +1,12 @@
+import variables from '../variables/variables'
 import reaction from './reaction'
 import symbolStringGenerator from '../functions/symbolStringGenerator'
 import request from 'request'
 import requestP from 'request-promise'
-
 import pollStore from '../variables/api/pollStore'
 pollStore();
+
+let bot = variables.bot
 
 let pollManager = {
     // хранилища опросов
@@ -13,20 +15,21 @@ let pollManager = {
 
     // создать новый опрос
     createPoll: (id, title, answers) => {
-        // массив с подсчетом голосов за каждый вариант
-        let votesAmount = []
         // массив, содержащий массивы id голосовавших за каждый вариант
         let votedUsers = []
+        // и массив, содержащий номер ответа голосовавшего
+        let votedUsersAnswer = []
+        // массив с кол-вом голосов за каждый вариант
+        let votesAmount = []
         // кнопки опроса
         let buttons = []
         // заполняем votesAmount нулями, votedUsers кол-вом массивов соответствующим кол-ву вариантов
         // и выстраиваем кнопки опроса
         for (let i = 0; i < answers.length; i++) {
             votesAmount[i] = 0
-            votedUsers[i] = []
             let buttonBlank = {
                 text: `${answers[i]} - ${votesAmount[i]}`,
-                callback_data: 'poll_'+id+'_'+i
+                callback_data: 'poll_' + id + '_' + i
             }
             buttons[i] = [buttonBlank]
         }
@@ -39,10 +42,10 @@ let pollManager = {
         }
         // формируем объект опроса для записи в БД
         let pollObject = {
-            id : id,
-            title : title,
-            answers : answers,
-            votes : { votesAmount : votesAmount, votedUsers : votedUsers }
+            id: id,
+            title: title,
+            answers: answers,
+            votes: { votedUsers: votedUsers, votedUsersAnswer: votedUsersAnswer, votesAmount: votesAmount }
         }
         // формируем настройки пост-запроса для добавления опроса в БД
         let options_post = {
@@ -55,73 +58,88 @@ let pollManager = {
         request.post('http://localhost:3012/pollstore', options_post);
         // возвращаем из метода все что нужно для того чтобы отослать опрос в телегу
         let pollProperties = {
-            pollObject : pollObject, 
-            options : options_poll
+            pollObject: pollObject,
+            options: options_poll
         }
         return pollProperties
     },
     // обновить опрос
     updatePoll: (msg, data) => {
         let id = data[1]
-        let answerNumber = data[2]
+        let userAnswer = data[2]
         let userId = msg.from.id
-        // let clickedPoll
-        // let userVotes
-        let result
-        
+        let clickedPoll
+        let buttons = []
+
         console.log('v update')
         requestP.get('http://localhost:3012/pollstore/' + id).then((pollObject) => {
-            result = JSON.parse(pollObject)
-            console.log(result)
-
-            // clickedPoll = result.poll
-            // userVotes = result.userVotes
-
-            // for (let i = 0; i < pollManager.pollStore.length; i++) {
-            //     if (pollManager.pollStore[i][0] == id) {
-            //         clickedPoll = pollManager.pollStore[i][1]
-            //         userVotes = pollManager.pollStore[i][2]
-            //         break
-            //     }
-            // }
+            clickedPoll = JSON.parse(pollObject)
+            let votes = clickedPoll.votes
+            let votedUsers = votes.votedUsers
+            let votedUsersAnswer = votes.votedUsersAnswer
+            let votesAmount = votes.votesAmount
+            let answers = clickedPoll.answers
+            let title = clickedPoll.title
             // если в списке проголосовавших человек уже есть
-            // if (userVotes[0].includes(userId)) {
-            //     let userPos = userVotes[0].indexOf(userId) // на той же позиции всегда находится и номер ответа
-            //     let lastAnswerNumber = userVotes[1][userPos]
-            //     // если он кликнул туда же, куда и в прошлый раз, убрать его голос
-            //     if (lastAnswerNumber == answerNumber) {
-            //         userVotes[0].splice(userPos, 1)
-            //         userVotes[1].splice(userPos, 1)
-            //         clickedPoll.votes[lastAnswerNumber]--
-            //         // если он кликнул в другой вариант, перезаписать результат голосования
-            //     } else {
-            //         userVotes[0].splice(userPos, 1)
-            //         userVotes[1].splice(userPos, 1)
-            //         clickedPoll.votes[lastAnswerNumber]--
-            //         userVotes[0].push(userId)
-            //         userVotes[1].push(answerNumber)
-            //         clickedPoll.votes[answerNumber]++
-            //     }
-            //     // если в списке голосовавших его не было, добавить
-            // } else {
-            //     userVotes[0].push(userId)
-            //     userVotes[1].push(answerNumber)
-            //     clickedPoll.votes[answerNumber]++
-            // }
+            if (votedUsers.includes(userId)) {
+                let userPos = votedUsers.indexOf(userId) // на той же позиции всегда находится и номер ответа
+                let userLastAnswer = votedUsersAnswer[userPos]
+                // если он кликнул туда же, куда и в прошлый раз, убрать его голос
+                if (userLastAnswer == userAnswer) {
+                    userVotes.splice(userPos, 1)
+                    votedUsersAnswer.splice(userPos, 1)
+                    votesAmount[userAnswer]--
+                    // если он кликнул в другой вариант, перезаписать результат голосования
+                } else {
+                    userVotes.splice(userPos, 1)
+                    votedUsersAnswer.splice(userPos, 1)
+                    votesAmount[userAnswer]--
+                    votedUsers.push(userId)
+                    votedUsersAnswer.push(userAnswer)
+                    votesAmount[userAnswer]++
+                }
+                // если в списке голосовавших его не было, добавить
+            } else {
+                votedUsers.push(userId)
+                votedUsersAnswer.push(userAnswer)
+                votesAmount[userAnswer]++
+            }
+            // выстраиваем кнопки опроса
+            for (let i = 0; i < answers.length; i++) {
+                let buttonBlank = {
+                    text: `${answers[i]} - ${votesAmount[i]}`,
+                    callback_data: 'poll_' + id + '_' + i
+                }
+                buttons[i] = [buttonBlank]
+            }
 
-            // clickedPoll.update_poll(msg)
+            let options_poll = {
+                reply_markup: JSON.stringify({
+                    inline_keyboard: buttons
+                })
+            }
+            let messageId = msg.message.message_id
+            let chatId = msg.message.chat.id
 
-            // let options_put = {
-            //     headers: {
-            //         'Content-Type': 'application/json',
-            //     },
-            //     body: JSON.stringify({
-            //         poll: clickedPoll,
-            //         userVotes: userVotes
-            //     })
-            // }
+            bot.editMessageText(title, {
+                message_id: messageId,
+                chat_id: chatId,
+                parse_mode: 'Markdown',
+                reply_markup: options_poll.reply_markup
+            })
 
-            // request.put('http://localhost:3012/pollstore', options_put);
+            let options_put = {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    title: title,
+                    answers: answers,
+                    votes: votes
+                })
+            }
+
+            request.put('http://localhost:3012/pollstore'+id, options_put);
         })
     },
     createReaction: (link, sendTo) => {
