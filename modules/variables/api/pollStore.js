bedae89fd915495080171fbda7edc579db671389
Klimenko Ivan <kli-van@yandex.ru>
2018-08-31 17:10:07 +0300

    database

--- modules/variables/api/pollStore.js
+++ modules/variables/api/pollStore.js
@@ -30,6 +30,7 @@ export default function pollStore() {
                 console.log(err)
                 return res.sendStatus(500)
             }
+            res.send(poll)
         })
     })
 }
