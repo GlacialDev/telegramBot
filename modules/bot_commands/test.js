276d9dc384343ce44f95822a978e9ac7b1220a0e
Glacial <kli-van@yandex.ru>
2018-09-09 01:16:48 +0300

    media-group

--- modules/bot_commands/test.js
+++ modules/bot_commands/test.js
@@ -7,5 +7,23 @@ export default function test() {
     bot.onText(/\/test/, (msg, match) => {
         if (authCheck(msg) != true) return
 
+
+        bot.sendMediaGroup(variables.creator, [
+            {
+              type: "photo",
+              media:
+                "https://memepedia.ru/wp-content/uploads/2017/04/450px-Sad_fox.jpg"
+            },
+            {
+              type: "photo",
+              media:
+                "https://bipbap.ru/wp-content/uploads/2017/05/VOLKI-krasivye-i-ochen-umnye-zhivotnye.jpg"
+            },
+            {
+              type: "photo",
+              media:
+                "https://img3.goodfon.ru/wallpaper/big/b/cf/kartinka-3d-raznocvetnyy-grib.jpg"
+            }
+          ]);
     });
 } 
