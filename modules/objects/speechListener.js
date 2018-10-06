773c9bcf7df1664ccf54bd7b8e32fbc57ece0bc4
Glacial <kli-van@yandex.ru>
2018-10-07 02:33:07 +0300

    ok we try to use yandex speechkit

--- modules/objects/speechListener.js
+++ modules/objects/speechListener.js
@@ -9,40 +9,39 @@ let speechListener = {
         return new Promise((resolve, reject) => {
             let filePath = bot.downloadFile(msg.voice.file_id, './data/download/voice/').then(
                 (filePath) => {
-                    
-
                     let options = {
                         method: 'POST',
                         host: 'asr.yandex.net',
                         contentType: msg.voice.mime_type,
                         transferEncoding: 'chunked',
-                        data: fs.readFile(filePath)
+                        data: ''
                     };
 
-                    let req = https.request(options, function (res) {
-                        console.log(res.statusCode);
-                        // res.on('data', function (d) {
-                        //     fs.createWriteStream('./data/download/voice/yandexSpeech.oga')
-                        //         .on('finish', function () {
-                        //             console.log(res)
-                        //         })
-                        //         .on('error', function (error) {
-                        //             if (error) console.log(error)
-                        //         })
-                        // });
-                    });
-                    req.end();
-
-                    req.on('error', function (e) {
-                        console.error(e);
-                    });
-
-                    resolve('загрузил')
+                    fs.createReadStream(filePath)
+                        .pipe(fs.createWriteStream(options.data))
+                        .on('finish', function () {
+                            console.log(options.data)
+                            resolve(options)
+                        })
+                        .on('error', function (error) {
+                            if (error) console.log(error)
+                        })
                 },
                 (e) => {
-                    console.log(e)
-                    reject('не загрузил')
-                })
+                    reject(e)
+                }).then(
+                    (options) => {
+                        let req = https.request(options, function (res) {
+                            console.log(res);
+                        });
+                        req.end();
+
+                        req.on('error', function (e) {
+                            console.error(e);
+                        });
+                    },
+                    (e) => console.log(e)
+                )
         })
     }
 }
