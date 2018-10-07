085bcdc6e9f7cd8729fa0a3cbecdc5b0fbbaa18a
Glacial <kli-van@yandex.ru>
2018-10-07 20:08:31 +0300

    yandex cool

--- modules/bot_commands/test.js
+++ modules/bot_commands/test.js
@@ -3,16 +3,16 @@ import authCheck from '../functions/authCheck'
 import config from '../secret/config';
 
 let bot = variables.bot
+let audioConverter = variables.audioConverter
 
 export default function test() {
     bot.onText(/\/test/, (msg, match) => {
         if (authCheck(msg) != true) return
 
-
-        if(config.azureKey !== undefined) {
-            bot.sendMessage(msg.chat.id, 'azurekey est')
-        } else {
-            bot.sendMessage(msg.chat.id, 'azurekey netu')
-        }
+        audioConverter("./data/download/voice/", "./data/converted/", {
+            progressBar: true
+        }).then(function () {
+            console.log("Done!");
+        });
     });
 } 
