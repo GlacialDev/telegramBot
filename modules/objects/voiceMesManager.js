c58fae74f66209a63b7772f67bbca0df128aff29
Glacial <kli-van@yandex.ru>
2018-10-11 18:43:21 +0300

    why promises is so unintuintive :(

--- modules/objects/voiceMesManager.js
+++ modules/objects/voiceMesManager.js
@@ -13,54 +13,56 @@ let voiceMesManager = {
     // грузим голосовое сообщение
     let filePath = bot.downloadFile(msg.voice.file_id, './data/download/voice/').then(
       (filePath) => {
-        let answer = ''
-        // делим путь
-        let regExpList = filePath.split(/\\/)
-        // вытягиваем имя файла
-        let inputFileName = regExpList[regExpList.length - 1]
-        // делим по точке на
-        let regExpFormat = inputFileName.split(/\./)
-        // расширение 
-        let inputFormat = regExpFormat[regExpFormat.length - 1]
-        // название
-        let inputName = regExpFormat[regExpFormat.length - 2]
-        // указываем необходимый формат
-        let outputFormat = 'mp3'
-        // получаем имя выходного файла
-        let outputFileName = 'output_' + inputName + '.' + outputFormat
+        return new Promise((resolve, reject) => {
+          let answer = ''
+          // делим путь
+          let regExpList = filePath.split(/\\/)
+          // вытягиваем имя файла
+          let inputFileName = regExpList[regExpList.length - 1]
+          // делим по точке на
+          let regExpFormat = inputFileName.split(/\./)
+          // расширение 
+          let inputFormat = regExpFormat[regExpFormat.length - 1]
+          // название
+          let inputName = regExpFormat[regExpFormat.length - 2]
+          // указываем необходимый формат
+          let outputFormat = 'mp3'
+          // получаем имя выходного файла
+          let outputFileName = 'output_' + inputName + '.' + outputFormat
 
-        ffMpegAudioProcess(inputFileName, outputFileName).then(
-          () => {
-            yandexSpeech.ASR({
-              developer_key: config.yandexSpeechKitKey,
-              file: `./data/download/voice/${outputFileName}`,
-              filetype: 'audio/x-mpeg-3'
-            }, function (err, httpResponse, xml) {
-              if (err) {
-                console.log(err);
-              } else {
-                // парсим xml достаем то что в тегах variant
-                let variantsList = xml.split(/<variant confidence="\d+.?\d+">(.+)<\/variant>/)
-                // берем ответ первого варианта
-                let textFromSpeechList = variantsList[0].split(/>(.+)</)
-                answer = textFromSpeechList[1]
-                // удаляем файлы ogg/mp3
-                fs.unlink(`./data/download/voice/${inputFileName}`, (err) => {
-                  if (err) throw err;
-                  console.log(inputFileName + " deleted");
-                });
-                fs.unlink(`./data/download/voice/${outputFileName}`, (err) => {
-                  if (err) throw err;
-                  console.log(outputFileName + " deleted");
-                });
-                console.log(answer + '--1')
-              }
-              console.log(answer + '--2')
-              resolve(answer)
+          ffMpegAudioProcess(inputFileName, outputFileName).then(
+            () => {
+              yandexSpeech.ASR({
+                developer_key: config.yandexSpeechKitKey,
+                file: `./data/download/voice/${outputFileName}`,
+                filetype: 'audio/x-mpeg-3'
+              }, function (err, httpResponse, xml) {
+                if (err) {
+                  console.log(err);
+                } else {
+                  // парсим xml достаем то что в тегах variant
+                  let variantsList = xml.split(/<variant confidence="\d+.?\d+">(.+)<\/variant>/)
+                  // берем ответ первого варианта
+                  let textFromSpeechList = variantsList[0].split(/>(.+)</)
+                  answer = textFromSpeechList[1]
+                  // удаляем файлы ogg/mp3
+                  fs.unlink(`./data/download/voice/${inputFileName}`, (err) => {
+                    if (err) throw err;
+                    console.log(inputFileName + " deleted");
+                  });
+                  fs.unlink(`./data/download/voice/${outputFileName}`, (err) => {
+                    if (err) throw err;
+                    console.log(outputFileName + " deleted");
+                  });
+                  console.log(answer+'--1')
+                }
+                console.log(answer+'--2')
+                resolve(answer)
+              })
+              console.log(answer+'--3')
             })
-            console.log(answer + '--3')
-          })
-        console.log(answer + '--4')
+          console.log(answer+'--4')
+        })
       })
   },
   speechFromText: (text) => {
