9178ca296f300b0f5172c3ce0e6ebe88c559cace
Glacial <kli-van@yandex.ru>
2018-10-19 23:06:54 +0300

    added ffmpeg

---
+++ modules/secret/config.js
@@ -0,0 +1,19 @@
+let config = {
+  "token" : null,
+  "azureKey" : null,
+  "dialogFlowClientAccessToken" : null,
+  "cloudConvertApiKey" : null,
+  "yandexSpeechKitKey" : null,
+  "authorizedUsers" : [
+      111111111,
+      111111111,
+      111111111,
+      222222222
+  ],
+  "adminUsers" : [
+      222222222
+  ],
+  "canadianEroId": -0000000000000
+}
+
+export default config;
